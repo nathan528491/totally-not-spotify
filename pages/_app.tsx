1ee264e719a2bc8a42bf9862be2e41541cbaf2e8
githubacc528491 <nathan.lancman@gmail.com>
2022-06-03 15:24:16 +0200

    ADD Start of the sidebar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import {ChakraProvider, extendTheme} from "@chakra-ui/react";
 import 'reset-css'
+import PlayerLayout from "../components/PlayerLayout";
 
 const theme = extendTheme({
     colors: {
@@ -29,7 +30,13 @@ const theme = extendTheme({
     }
 })
 function MyApp({Component, pageProps}) {
-    return <Component {...pageProps} />
+    return (
+        <ChakraProvider theme={theme}>
+            <PlayerLayout>
+                <Component {...pageProps} />
+            </PlayerLayout>
+        </ChakraProvider>
+    )
 }
 
 export default MyApp
